a77c84117118e5d908dba2bec863ef2dc7fce4ad
alangirish <athiralan0@gmail.com>
2024-08-09 09:15:49 +0530

    Responsiveness Improved

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,8 @@ function Home() {
       <h1 className="text-4xl">Welcome to the Quiz App</h1>
       <p className="mt-4 mb-4">Test your knowledge or create your own quiz!</p>
       <div className='p-4 flex items-center justify-between'>
-       <button className='bg-gradient-to-r from-fuchsia-500 to-cyan-500 rounded-full w-32 sm:w-40 lg:w-48 text-lg p-2 transition duration-300 ease-in-out hover:scale-105 text-zinc-800'><Link to='/quiz'>Start the quiz </Link></button> 
-       <button className='bg-gradient-to-r from-slate-300 to-slate-500 rounded-full w-32 sm:w-40 lg:w-48 text-lg p-2 transition duration-300 ease-in-out hover:scale-105 text-zinc-800'><Link to='/create-quiz'>Create a quiz </Link></button> 
+       <button className='bg-gradient-to-r from-fuchsia-500 to-cyan-500 rounded-full w-32 sm:w-40 lg:w-48 sm:text-sm lg:text-lg p-2 transition duration-300 ease-in-out hover:scale-105 text-zinc-800'><Link to='/quiz'>Start the quiz </Link></button> 
+       <button className='bg-gradient-to-r from-slate-300 to-slate-500 rounded-full w-32 sm:w-40 lg:w-48 sm:text-sm lg:text-lg p-2 transition duration-300 ease-in-out hover:scale-105 text-zinc-800'><Link to='/create-quiz'>Create a quiz </Link></button> 
       </div>
       
     </div>
